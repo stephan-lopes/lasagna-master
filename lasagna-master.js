2e76c0875e92704c645f3046a136bae04b4affba
stephan-lopes <40838038+stephan-lopes@users.noreply.github.com>
2023-08-17 08:31:07 -0300

    feat: complete solution for this exercise

--- lasagna-master.js
+++ lasagna-master.js
@@ -10,3 +10,45 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime) {
+    return remainingTime === 0 ? "Lasagna is done." : remainingTime === undefined ? "You forgot to set the timer." : "Not done, please wait.";
+}
+
+export function preparationTime(layers, qtd = 2) {
+    return layers.length * qtd
+}
+
+export function quantities(layers) {
+    const quantitiesToMeal = {
+        'noodles' : 50, //50g
+        'sauce' : 0.2 //0.2l
+    }
+
+    return {
+        noodles : preparationTime(
+            layers.filter(obj => obj === 'noodles'), 
+            quantitiesToMeal.noodles
+        ),
+
+        sauce : preparationTime(
+            layers.filter(obj => obj === 'sauce'),
+            quantitiesToMeal.sauce
+        )
+    }
+}
+
+export function addSecretIngredient(friendsList, myList) {
+    myList.push(friendsList[friendsList.length-1])
+}
+
+
+// It's possible to use object destructuring as well. 
+// However, this approach is less dynamic.
+export function scaleRecipe(recipe, portions) {
+    const personPerPortion = portions/2
+    
+    return  Object.fromEntries(
+        Object.entries(recipe).map(([key, value]) => [key, value * personPerPortion])
+    );
+} 
